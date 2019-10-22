12a40dda473e6f91e0d40a50a9a3ffbc8c665a9d
garim <garim10@naver.com>
2019-10-22 21:33:37 +0900

    remove directory

--- Basic/1/q1001.py
+++
@@ -1,6 +0,0 @@
-# https://www.acmicpc.net/problem/1001
-
-x = input().split(' ')
-a = int(x[0])
-b = int(x[1])
-print(a - b)
--- Basic/2/q10817.py
+++
@@ -1,7 +0,0 @@
-# https://www.acmicpc.net/problem/10817
-
-# x = list(map(int, input().split())) # 이것도 가능
-list = input().split(' ')
-x = [int(i) for i in list]
-x.sort()
-print(x[1])

7a2d3b84ea1edd7a5b131e53382074138ee190f8
garim <garim10@naver.com>
2019-10-23 18:54:35 +0900

    최소, 최대

---
+++ Basic/4/q10818.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/10818
+
+import sys
+
+def max_number(list):
+    max_n = -1000000
+    for i in list:
+        if max_n < i:
+            max_n = i
+    return max_n
+
+def min_number(list):
+    min_n = 1000000
+    for i in list:
+        if min_n > i:
+            min_n = i
+    return min_n
+
+n = int(sys.stdin.readline().rstrip())
+n_list = list(map(int, sys.stdin.readline().split()))
+
+print(min_number(n_list), max_number(n_list))

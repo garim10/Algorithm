37c16f1b63d2439fa915edaaa78e04bd0524c738
garim10 <56871437+garim10@users.noreply.github.com>
2019-11-10 21:39:51 +0900

    queue

---
+++ Study/14_큐.cpp
@@ -0,0 +1,23 @@
+// 참고: http://blog.naver.com/ndb796/221230944729
+// 큐(Queue) - FIFO 구조 
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(void) {
+	queue<int> q;
+	q.push(7);
+	q.push(5);
+	q.push(4);
+	q.pop();
+	q.push(6);
+	q.pop();
+	while(!q.empty()) {
+		cout << q.front() << ' '; // 가장 앞에 있는 값 출력 
+		q.pop();
+	}
+	
+	return 0;
+} 
